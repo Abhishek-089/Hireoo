6dd456567c98bd1101d75eaa04449a3e36c39576
Abhishek-089 <abhishek1811@gmail.com>
2026-02-03 17:17:49 +0530

    fix-robo

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXTAUTH_URL || 'https://www.hireoo.in'
+    const baseUrl = 'https://www.hireoo.in'
 
     return {
         rules: {
